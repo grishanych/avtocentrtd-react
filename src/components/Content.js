332265979ab5b9f6e29407e954ff55997e9af168
grishanych <alsolo2011@gmail.com>
2022-07-11 21:34:17 +0300

    Create content with images

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,27 +1,71 @@
 import React from "react";
 import './Content.scss';
-import Wheel from '../images/wheel.png';  
-import Сhassis from '../images/chassis.png'; 
-import Generator from '../images/generator.png'; 
-import AirCond from '../images/airCond.png';  
-import CompDiagnostik from '../images/compDiagnostik.png'; 
+import Wheel from '../images/wheel.png';
+import Сhassis from '../images/chassis.png';
+import Generator from '../images/generator.png';
+import AirCond from '../images/airCond.png';
+import CompDiagnostik from '../images/compDiagnostik.png';
 import Disc from '../images/disc.png'; //change img
-import Grm from '../images/grm.png'; 
+import Grm from '../images/grm.png';
 import Cleaning from '../images/cleaning.png';
+import ContentBG from '../images/bg2.jpg';
 
 
-function Content(){
-    return(
-        <div className="content">
+function Content() {
+    return (
+        <div className="content"  style={{ backgroundImage:`url(${ContentBG})` }}>
             <div className="content-images">
-            <img src={Wheel} />
-            <img src={Сhassis} />
-            <img src={Generator} />
-            <img src={AirCond} />
-            <img src={CompDiagnostik} />
-            <img src={Disc} />
-            <img src={Grm} />
-            <img src={Cleaning} />
+                <div className="first-line">
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p>шиномонтаж</p>
+                        </div>
+                        <img src={Wheel} />
+                    </div>
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p>ремонт ходової</p></div>
+                        <img src={Сhassis} />
+                    </div>
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p>ремонт стартерів та генераторів</p>
+                        </div>
+                        <img src={Generator} /></div>
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p>заправка та ремонт кондиціонерів</p>
+                        </div>
+                        <img src={AirCond} />
+                    </div>
+                </div>
+
+                <div className="second-line">
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p >комп'ютерна діагностика</p>
+                        </div>
+                        <img src={CompDiagnostik} />
+                    </div>
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p >рихтовка дистів, аргонна зварка</p>
+                        </div>
+                        <img src={Disc} />
+                    </div>
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p >заміна ремнів грм</p>
+                        </div>
+                        <img src={Grm} />
+                    </div>
+                    <div className="image-content-hover">
+                        <div className="text-for-content-images">
+                            <p >хімчистка салону</p>
+                        </div>
+                        <img src={Cleaning} />
+                    </div>
+                </div>
             </div>
         </div>
     )
